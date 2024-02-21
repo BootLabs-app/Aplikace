c7c23b97b1cc341637d22ea190671ef699742c18
haxxTom <tomas.bilek.2006@gmail.com>
2024-02-21 11:57:10 +0100

    vylepšení

--- dropdown.js
+++ dropdown.js
@@ -1,7 +1,23 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-  }
+}
   
-  function closeNav() {
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+}
+
+var i = 0;
+var txt = 'Aplikace a bezpečnost na míru pro vaši firmu';
+var speed = 50;
+
+window.onload = function typetext() { 
+  typeWriter();
+}
+function typeWriter(){
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
   }
+}
+  
